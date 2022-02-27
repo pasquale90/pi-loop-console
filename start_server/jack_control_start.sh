786e54f2c82125051cefb86d8a8ee0b666323d66
pasquale90 <melissaspaschalis@gmail.com>
2022-02-27 11:23:49 +0000

    removed scripts folder

---
+++ start_server/jack_control_start.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+
+jack_control start ds alsa dps rate 44100 dps period 512 eps realtime true
+
+
---
+++ start_server/start_jack.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+#cat /proc/asound/cards
+
+#jackd --verbose -R -v -t10000 -dalsa -d"hw:K6" -r16000 -p512
+jackd --realtime --verbose -t10000 -dalsa -dhw:K6 -r16000 -p512
+#jackd --realtime --verbose -t10000 -dalsa -dhw:K6 -r44100 -p128
+
+#for debug
+#tail -f ~/.log/jack/jackdbus.log
+
+#jack_control stop
+#killall jackd
+#sudo killall -9 jackdbus
+#sudo killall -9 jackd
+#jack_control exit
+#jack_control start
