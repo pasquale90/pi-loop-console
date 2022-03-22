3c3c01d59de612f7c4a64083ef57982b59f3cb4c
pasquale90 <melissaspaschalis@gmail.com>
2022-03-22 07:59:11 +0200

    modified paths.py

--- d2m/paths.py
+++ d2m/paths.py
@@ -1,23 +1,23 @@
 import os
 
+PWD=os.getcwd()
+datapath=os.path.join(PWD,"dataset/e-gmd")
+csvpath=os.path.join(datapath,"e-gmd-v1.0.0.csv")
+
 def sample_files():
-  samplepath=PWD+"/samples/"
+  samplepath=os.path.join(PWD,"dataset/samples")
   sample="220_rock-halftime_140_fill_4-4_6"
-  midisample=samplepath+sample+".midi"
-  audiosample=samplepath+sample+".wav"
+  midisample=os.path.join(samplepath,sample+".midi")
+  audiosample=os.path.join(samplepath,sample+".wav")
   return midisample,audiosample
 
 def get_fold_path(fold):
-  filepath=datapath+"drummer"+str(fold)+"/"
+  filepath=os.path.join(datapath,"drummer"+str(fold))
   return filepath
 
 def set_session_path(foldpath,sessionpath):
-  return foldpath+sessionpath
+  return os.path.join(foldpath,sessionpath)
 
 def get_session_path(foldpath):
   return os.listdir(foldpath)
-
-PWD=os.getcwd()
-datapath=PWD+"/e-gmd/"
-csvpath=datapath+"e-gmd-v1.0.0.csv"
 
