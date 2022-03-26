8b939c637306995e2de9cab845338bde37283a41
pasquale90 <melissaspaschalis@gmail.com>
2022-03-26 10:49:56 +0200

    modified midifile for reading all the mmiditracks

--- d2m/data/midifile.py
+++ d2m/data/midifile.py
@@ -1,4 +1,5 @@
 import mido 
+from data.note import Note
 
 class Midifile:
   def __init__(self,filepath):
@@ -20,50 +21,52 @@ class Midifile:
   def parse_midi_messages(self):
 
     elapsed_ticks=0
-    for m,message in enumerate(self.midifile.tracks[0]):
-      message_type=message.type
-      
-      elapsed_ticks+=message.time
-      print(f'message {m} , time= {message.time} elapsed_ticks={elapsed_ticks}')
-
-      if message_type=="set_tempo":
-        self.tempo=message.tempo
-        self.bpm=int(mido.tempo2bpm(self.tempo))
-
-      elif message_type=="time_signature":
-        self.rythm_numerator=message.numerator
-        self.rythm_denominator=message.denominator
-        self.clocks_per_click=message.clocks_per_click
-        self.notated_32nd_notes_per_beat=message.notated_32nd_notes_per_beat
+    for mdt in range(len(self.midifile.tracks)):
+      for m,message in enumerate(self.midifile.tracks[mdt]):
+        message_type=message.type
+        elapsed_ticks+=message.time
         
-      elif message_type=="key_signature":
-        self.key=message.key
-      
-      elif message_type=="end_of_track":
-        self.ending_tick=message.time
-        self.total_ticks=elapsed_ticks
+        print(f'message {m} , time= {message.time} elapsed_ticks={elapsed_ticks}')
 
-      elif message_type=="note_on":
-        channel=message.channel 
-        pitch=message.note
-        velocity=message.velocity
-        time=message.time
-        #duration=self.midifile.tracks[0][m+1].time
-        #print(f'duration={self.midifile.tracks[0][m+1].time}')
-        timestamp=elapsed_ticks
-        timestamp_sec=self.convert_tic2sec(timestamp)
-        if velocity!=0:
-          temp_counter=1
-          duration=0
-          for msg in self.midifile.tracks[0][m+1:]:
-            duration+=msg.time
-            if msg.type=="note_on" and msg.note==pitch and msg.velocity==0:
-              new_note=Note(channel,pitch,velocity,timestamp,timestamp_sec,duration,self.convert_tic2sec(duration))
-              self.notes.append(new_note)
-              break
-      else:
-        #print(f"Unknown message {message}")
-        pass
+        if message_type=="set_tempo":
+          self.tempo=message.tempo
+          self.bpm=int(mido.tempo2bpm(self.tempo))
+
+        elif message_type=="time_signature":
+          self.rythm_numerator=message.numerator
+          self.rythm_denominator=message.denominator
+          self.clocks_per_click=message.clocks_per_click
+          self.notated_32nd_notes_per_beat=message.notated_32nd_notes_per_beat
+        
+        elif message_type=="key_signature":
+          self.key=message.key
+      
+        elif message_type=="end_of_track":
+          self.ending_tick=message.time
+          self.total_ticks=elapsed_ticks
+
+        elif message_type=="note_on":
+          channel=message.channel 
+          pitch=message.note
+          velocity=message.velocity
+          time=message.time
+          #duration=self.midifile.tracks[0][m+1].time
+          #print(f'duration={self.midifile.tracks[0][m+1].time}')
+          timestamp=elapsed_ticks
+          timestamp_sec=self.convert_tic2sec(timestamp)
+          if velocity!=0:
+
+            temp_counter=1
+            duration=0
+            for msg in self.midifile.tracks[mdt][m+1:]:
+              duration+=msg.time
+              if msg.type=="note_on" and msg.note==pitch and msg.velocity==0:
+                new_note=Note(channel,pitch,velocity,timestamp,timestamp_sec,duration,self.convert_tic2sec(duration))
+                self.notes.append(new_note)
+                break
+        else:
+          #print(f"Unknown message {message}")
+          pass
 
   def get_midifile(self):
     return self.midifile
