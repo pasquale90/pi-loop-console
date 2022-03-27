8f5e2687e4d4512525735962cfdb4e10fb1417c4
pasquale90 <melissaspaschalis@gmail.com>
2022-03-27 12:31:20 +0300

    removed helper functions to data/utils - pending cleaning

--- d2m/paths.py
+++ d2m/paths.py
@@ -45,28 +45,6 @@ def get_data_subpaths(datapath):
       pass
   return csv_path,folders
 
-def read_csv(csv_path):
-  midipaths,audiopaths,bpms,durations,splits,beat_types,sessions=[],[],[],[],[],[],[]
-  csv=pd.read_csv(csv_path)  
-  #print(csv.shape)
-  idxs = csv.index.values.tolist()
-  for i,idx in enumerate(idxs):
-    #print(f'index {idx}, value {csv.loc[idx,:]}')
-    midipaths.append(os.path.join(datapath,csv.loc[idx,"midi_filename"]))
-    #audio_filename=csv.loc[idx,"audio_filename"])
-    try:
-      audiopaths.append(os.path.join(datapath,csv.loc[idx,"audio_filename"]))
-    except TypeError:
-      audiopaths.append("-")
-    bpms.append(csv.loc[idx,"bpm"])
-    durations.append(csv.loc[idx,"duration"])
-    splits.append(csv.loc[idx,"split"])
-    beat_types.append(csv.loc[idx,"beat_type"])
-    sessions.append(csv.loc[idx,"session"])
-  return midipaths,audiopaths,bpms,durations,splits,beat_types,sessions
-
-
-
 #def print_paths_demo():
 def get_fold_path(fold):
   filepath=os.path.join(datapath,"drummer"+str(fold))
@@ -75,27 +53,6 @@ def set_session_path(foldpath,sessionpath):
   return os.path.join(foldpath,sessionpath)
 def get_session_path(foldpath):
   return os.listdir(foldpath)
-def pathscan(num_folds=10):
-  audiofiles,midifiles=[],[]
-  for f in range(1,num_folds+1):
-    if (f!=2):
-      #print("\n\nFOLD /#"+str(f))
-      #print(os.listdir(paths.get_fold_path(f)))
-      foldpath=get_fold_path(f)
-      #print(f'foldpath {foldpath}')
-      session_path=get_session_path(foldpath)
-      for sespath in session_path:
-        curr_path=set_session_path(foldpath,sespath)
-        #print(curr_audiopath)
-        for file in os.listdir(curr_path):
-          #print(f'audiofile:{file},\tcurr_audiopath:{curr_path},\tsespath:{sespath}')
-          #print(audiofile)
-          ext=file.split(".")[-1]
-          if (ext=="wav"):
-            audiofiles.append(curr_path+'/'+file)
-          if (ext=="midi"):
-            midifiles.append(curr_path+'/'+file)
-  return audiofiles, midifiles
 
 #method for returning abs path for each path readed from the csv
 def get_abs_datapath(filepath):
