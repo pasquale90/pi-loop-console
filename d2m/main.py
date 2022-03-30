b7ebedf4aeb9317a2fdad6c2803d470d3e05eef8
pasquale90 <melissaspaschalis@gmail.com>
2022-03-30 22:36:48 +0300

    checking calc_peaks functionality

--- d2m/main.py
+++ d2m/main.py
@@ -5,7 +5,7 @@ import librosa
 from tqdm import tqdm
 
 from paths import *
-from data import midifile,audio,utils
+from data import midifile,audio,utils,waveform
 
 #scan folders
 csv_path,folders=get_data_subpaths(datapath)
@@ -14,8 +14,6 @@ print(f'csv of shape \t {len(csv.index.values)} x {len(csv.columns.values)}')
 print(f'rows {csv.index.values}')
 print(f'columns {csv.columns.values}')
 
-
-
 print_thress=100
 correct,errors=0,0
 for i in tqdm(range (csv.shape[0])):
@@ -23,36 +21,24 @@ for i in tqdm(range (csv.shape[0])):
   audio_relpath=csv['audio_filename'].loc[csv.index[i]]
 
   midi=midifile.Midifile(get_abs_datapath(midi_relpath))
-  aud=audio.Audio(get_abs_datapath(audio_relpath),8000)
-  aud=aud.get_raw()
-  
+  aud=audio.Audio(get_abs_datapath(audio_relpath),2000)
+  raw=aud.get_raw()
 
-  timelength_diff=abs(midi.get_total_seconds()-len(aud)/8000)
-  if timelength_diff!=0:
-    print(f'ERROR timelength_diff : file {midi_relpath} --> midi {midi.get_total_seconds()} != {len(aud)/8000}')
-    errors+=1
-  else:
-    correct+=1
 
-  """check bpm signature
-  correct,approx,errors=0,0,0
-  bpm_diff=abs(midi.get_bpm()-csv['bpm'].loc[i])
-  if bpm_diff==1 or bpm_diff==2:
-    print(f'APPROX midifile {midifile} --> bpm {midi.get_bpm()} != {csv['bpm'].loc[i]}')
-    approx+=1
-  elif (bpm_diff>2):
-    print(f'ERROR midifile {midifile} --> bpm {midi.get_bpm()} != {csv['bpm'].loc[i]}')
-    errors+=1
-  else:
-    correct+=1
-  """
+
+  #"" temp
+  savefpath=os.path.join('/data/pl/pi-loop-console/d2m/tempfiles_/audios/',str(i)+'.wav')
+  print(savefpath)
+  aud.temp_save(savefpath)
   
-  if i >print_thress:
+  #env=aud.get_envelop()
+  wf = waveform.Waveform(get_abs_datapath(audio_relpath))
+  #wf.save('/data/pl/pi-loop-console/d2m/tempfiles_/waveforms/',str(i)+'.png')
+  #""
+
+
+  if i>20:
     break
-    
-#print(f'from {csv.shape[0]} midifiles, there are {correct}({correct/csv.shape[0]*100}%) correct, {approx}({approx/csv.shape[0]*100}%) approx and {errors}({errors/csv.shape[0]*100}%) errors')
-print(f'from {csv.shape[0]} midifiles, there are {correct}({round(correct/csv.shape[0]*100,2)}%) correct and {errors}({round(errors/print_thress*100,2)}%) errors')
-
 
 audiofiles, midifiles=utils.pathscan(len(folders))
 print(len(audiofiles), len(midifiles))
