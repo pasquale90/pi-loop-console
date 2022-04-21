9411527aec246cd4d647b3b17ce5044053cbe1c6
pasquale90 <melissaspaschalis@gmail.com>
2022-04-21 16:59:01 +0300

    created segment - dataloader preparation

--- d2m/data/segment.py
+++ d2m/data/segment.py
@@ -1,18 +1,112 @@
+from paths import *
+from data import midifile,audio,utils,waveform
+import os
+import mido #import MidiFile, tempo2bpm, tick2second
+import pandas as pd
+import librosa
+from tqdm import tqdm
+import math 
 
-class Segment:
-    def __init__(self,audiotrack,miditrack):
-        #self.musmeter=denominator
-        #self.audiotrack=audiotrack
-        #self.miditrack=miditrack
-        
-    def audio2frames(self):
-        frames = []
-        #get windows out of the raw waveform
-        for count_frames,(start,end) in enumerate(_windows(raw,window_size,hop_length)):
-            if(len(raw[start:end]) == window_size):
-                print(start,end)
-                frame = raw[start:end]#rectangular window
+def audio2frames(self):
+  frames = []
+  #get windows out of the raw waveform
+  for count_frames,(start,end) in enumerate(_windows(raw,window_size,hop_length)):
+    if(len(raw[start:end]) == window_size):
+      #print(start,end)
+      frame = raw[start:end]#rectangular window
+
+def create_segments(csv,folders): 
+  
+
+  for i in tqdm(range (csv.shape[0])):
+    midi_relpath=csv['midi_filename'].loc[csv.index[i]]
+    audio_relpath=csv['audio_filename'].loc[csv.index[i]]
+
+    midi=midifile.Midifile(get_abs_datapath(midi_relpath))
+    aud=audio.Audio(get_abs_datapath(audio_relpath),2000)
+    raw=aud.get_raw()
+
+    midi_tempo=midi.get_tempo()
+    midi_bpm=midi.get_bpm()
+    midi_numer=midi.get_rythm_numerator()
+    midi_denom=midi.get_rythm_denominator()
+    midi_note0=midi.get_note(0)
+    midi_numnotes=midi.get_num_notes()
+    midi_totalticks=midi.get_total_ticks()
+    midi_totalseconds=midi.get_total_seconds()
+    midi_mspertick=midi.get_ms_per_tick()
+    midi_convert_tick_to_second=midi.convert_tic2sec(512)
+    midi_ppq=midi.get_ppq()
+  
+    print(f"#### midi_relpath #### \n{midi_relpath}")
+    print(f"midi_tempo {midi_tempo}")
+    print(f"midi_bpm {midi_bpm}")
+    print(f"midi_numer {midi_numer}")
+    print(f"midi_denom {midi_denom}")
+    print(f"midi_note0 {midi_note0}")
+    print(f"midi_numnotes {midi_numnotes}")
+    print(f"midi_totalticks {midi_totalticks}")
+    print(f"midi_totalseconds {midi_totalseconds}")
+    print(f"midi_mspertick {midi_mspertick}")
+    print(f"midi_convert_tick_to_second {midi_convert_tick_to_second}")
+    print(f"midi_ppq {midi_ppq}")
+  
+  
+    audio_sr=aud.get_sampling_rate()
+    audio_ismono=aud.is_mono()
+    audio_duration=aud.get_duration_sec()
+    print(f"audio_sr {audio_sr}")
+    print(f"audio_ismono {audio_ismono}")
+    print(f"audio_duration {audio_duration}")
+
+
+    """ temp
+    savefpath=os.path.join('/data/pl/pi-loop-console/d2m/tempfiles_/audios/',str(i)+'.wav')
+    print(savefpath)
+    aud.temp_save(savefpath)
+  
+    #env=aud.get_envelop()
+    wf = waveform.Waveform(get_abs_datapath(audio_relpath))
+    #wf.save('/data/pl/pi-loop-console/d2m/tempfiles_/waveforms/',str(i)+'.png')
+    """
+
+    #working on segments --> pass them on segment class
+    quarter_length_secs=midi_ppq*midi_mspertick*midi_numer/1000
+    print(f"quarter_length_secs {quarter_length_secs}")
+    #we need 4 segments
+    segment_length_secs=4*quarter_length_secs
+    print(f"segment_length_secs {segment_length_secs}")
+    # how many segments can be produced by each sample
+    avail_seconds=min(midi_totalseconds,audio_duration)
+    print(f"avail_seconds {avail_seconds}")
+    avail_segments=math.floor(avail_seconds/segment_length_secs)
+    print(f"avail_segments {avail_segments}")
+    #crop audio
+    ## calc audio samples
+    audiosamples_per_segment=int(segment_length_secs*audio_sr)
+    print(f"audiosamples_per_segment {audiosamples_per_segment}")
+    ## create audio segments
+    for seg_step in range(avail_segments):
     
+      ### audio samples
+      from_audiosample=seg_step*audiosamples_per_segment
+      upto_audiosample=(seg_step+1)*audiosamples_per_segment
+      print(f"from_audiosample {from_audiosample}")
+      print(f"upto_audiosample {upto_audiosample}")
+      segment=raw[from_audiosample:upto_audiosample]
+      #break in windows -- for feeding the model
+    
+      ### midi samples (pending...)
+    
+
+
+    if i>2:
+      break
+
+  audiofiles, midifiles=utils.pathscan(len(folders))
+  print(len(audiofiles), len(midifiles))
+  #return segments,labels
+
 '''
 #define analysis parameters
 def analysis_parameters():
