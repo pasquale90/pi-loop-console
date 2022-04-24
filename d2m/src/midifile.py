e7b7ac0df28202255c7807979cfd9632ccc33c61
pasquale90 <melissaspaschalis@gmail.com>
2022-04-24 17:37:23 +0300

    changed folder structure

---
+++ d2m/src/midifile.py
@@ -0,0 +1,96 @@
+import mido 
+from note import Note
+
+class Midifile:
+  def __init__(self,filepath):
+    self.midifile=mido.MidiFile(filepath)
+    self.PPQ=self.midifile.ticks_per_beat
+    self.notes=[]
+    
+    self._parse_midi_messages()
+    
+
+    #self.total_ticks=self.set_length_ticks()
+    #self.ending_tick=0
+
+   #not explicitly required yet
+    #self.key=''
+    #self.clocks_per_click=0
+    #self.notated_32nd_notes_per_beat=0
+  
+  def _parse_midi_messages(self):
+
+    elapsed_ticks=0
+    for mdt in range(len(self.midifile.tracks)):
+      for m,message in enumerate(self.midifile.tracks[mdt]):
+        
+        message_type=message.type
+        elapsed_ticks+=message.time
+        #print(message, elapsed_ticks)
+        #print(f'message {m} , time= {message.time} elapsed_ticks={elapsed_ticks}')
+
+        if message_type=="set_tempo":
+          self.tempo=message.tempo
+          self.bpm=round(mido.tempo2bpm(self.tempo))
+
+        elif message_type=="time_signature":
+          self.rythm_numerator=message.numerator
+          self.rythm_denominator=message.denominator
+          self.clocks_per_click=message.clocks_per_click
+          self.notated_32nd_notes_per_beat=message.notated_32nd_notes_per_beat
+        
+        elif message_type=="key_signature":
+          self.key=message.key
+      
+        elif message_type=="end_of_track":
+          self.ending_tick=message.time
+          self.total_ticks=elapsed_ticks
+
+        elif message_type=="note_on":
+          channel=message.channel 
+          pitch=message.note
+          velocity=message.velocity
+          time=message.time
+          #duration=self.midifile.tracks[0][m+1].time
+          #print(f'duration={self.midifile.tracks[0][m+1].time}')
+          timestamp=elapsed_ticks
+          timestamp_sec=self.convert_tic2sec(timestamp)
+          #print(pitch, timestamp_sec)
+          if velocity!=0:
+
+            temp_counter=1
+            duration=0
+            for msg in self.midifile.tracks[mdt][m+1:]:
+              duration+=msg.time
+              if msg.type=="note_on" and msg.note==pitch and msg.velocity==0:
+                new_note=Note(channel,pitch,velocity,timestamp,timestamp_sec,duration,self.convert_tic2sec(duration))
+                self.notes.append(new_note)
+                break
+        else:
+          #print(f"Unknown message {message}")
+          pass
+
+  def get_midifile(self):
+    return self.midifile
+  def get_ppq(self):
+    return self.PPQ
+  def get_tempo(self):
+    return self.tempo
+  def get_bpm(self):
+    return self.bpm
+  def get_rythm_numerator(self):
+    return self.rythm_numerator
+  def get_rythm_denominator(self):
+    return self.rythm_denominator
+  def get_note(self,idx):
+    return self.notes[idx]
+  def get_num_notes(self):
+    return len(self.notes)
+  def get_total_ticks(self):
+    return self.total_ticks
+  def get_total_seconds(self):
+    return mido.tick2second(self.total_ticks,self.PPQ,self.tempo)
+  def get_ms_per_tick(self):
+    return (60000 / (self.bpm * self.PPQ))
+  def convert_tic2sec(self,tick):
+    return mido.tick2second(tick,self.PPQ,self.tempo)
---
+++ d2m/src/utils.py
@@ -0,0 +1,44 @@
+import os
+from paths import get_fold_path,set_session_path,get_session_path
+
+def pathscan(num_folds=10):
+  audiofiles,midifiles=[],[]
+  for f in range(1,num_folds+1):
+    if (f!=2):
+      #print("\n\nFOLD /#"+str(f))
+      #print(os.listdir(paths.get_fold_path(f)))
+      foldpath=get_fold_path(f)
+      #print(f'foldpath {foldpath}')
+      session_path=get_session_path(foldpath)
+      for sespath in session_path:
+        curr_path=set_session_path(foldpath,sespath)
+        #print(curr_audiopath)
+        for file in os.listdir(curr_path):
+          #print(f'audiofile:{file},\tcurr_audiopath:{curr_path},\tsespath:{sespath}')
+          #print(audiofile)
+          ext=file.split(".")[-1]
+          if (ext=="wav"):
+            audiofiles.append(curr_path+'/'+file)
+          if (ext=="midi"):
+            midifiles.append(curr_path+'/'+file)
+  return audiofiles, midifiles
+  
+def read_csv(csv_path):
+  midipaths,audiopaths,bpms,durations,splits,beat_types,sessions=[],[],[],[],[],[],[]
+  csv=pd.read_csv(csv_path)  
+  #print(csv.shape)
+  idxs = csv.index.values.tolist()
+  for i,idx in enumerate(idxs):
+    #print(f'index {idx}, value {csv.loc[idx,:]}')
+    midipaths.append(os.path.join(datapath,csv.loc[idx,"midi_filename"]))
+    #audio_filename=csv.loc[idx,"audio_filename"])
+    try:
+      audiopaths.append(os.path.join(datapath,csv.loc[idx,"audio_filename"]))
+    except TypeError:
+      audiopaths.append("-")
+    bpms.append(csv.loc[idx,"bpm"])
+    durations.append(csv.loc[idx,"duration"])
+    splits.append(csv.loc[idx,"split"])
+    beat_types.append(csv.loc[idx,"beat_type"])
+    sessions.append(csv.loc[idx,"session"])
+  return midipaths,audiopaths,bpms,durations,splits,beat_types,sessions
