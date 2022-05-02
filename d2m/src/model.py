5e26f8aa70828c8cce9518f296c091bf6b982d94
pasquale90 <melissaspaschalis@gmail.com>
2022-05-03 00:06:27 +0300

    added a model for furture experimentation

--- d2m/src/model.py
+++ d2m/src/model.py
@@ -1,6 +1,7 @@
 #@title simple lstm
 class simplelstm(nn.Module):
     def __init__(self, input_dim, hidden_dim, seq_len, num_layers=2, batch_size=16, output_dim=50 ):
+    #num_classes, input_size, hidden_size, num_layers, seq_length
         super(simplelstm, self).__init__()
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
@@ -40,3 +41,34 @@ class simplelstm(nn.Module):
         #genre_scores = F.log_softmax(x, dim=1)
         #print('softmax',genre_scores.shape)
         return genre_scores
+
+
+# check tutorial :
+# https://cnvrg.io/pytorch-lstm/
+class LSTM1(nn.Module):
+    def __init__(self, num_classes, input_size, hidden_size, num_layers, seq_length):
+        super(LSTM1, self).__init__()
+        self.num_classes = num_classes #number of classes
+        self.num_layers = num_layers #number of layers
+        self.input_size = input_size #input size
+        self.hidden_size = hidden_size #hidden state
+        self.seq_length = seq_length #sequence length
+
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
+                          num_layers=num_layers, batch_first=True) #lstm
+        self.fc_1 =  nn.Linear(hidden_size, 128) #fully connected 1
+        self.fc = nn.Linear(128, num_classes) #fully connected last layer
+
+        self.relu = nn.ReLU()
+    
+    def forward(self,x):
+        h_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)) #hidden state
+        c_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)) #internal state
+        # Propagate input through LSTM
+        output, (hn, cn) = self.lstm(x, (h_0, c_0)) #lstm with input, hidden, and internal state
+        hn = hn.view(-1, self.hidden_size) #reshaping the data for Dense layer next
+        out = self.relu(hn)
+        out = self.fc_1(out) #first Dense
+        out = self.relu(out) #relu
+        out = self.fc(out) #Final Output
+        return out
