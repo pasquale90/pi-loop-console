1de157d2e8ee6bee2bc3d292f9155e51dd05df1d
pasquale90 <melissaspaschalis@gmail.com>
2022-05-05 08:30:59 +0300

    added normalization method

--- d2m/src/audio.py
+++ d2m/src/audio.py
@@ -1,10 +1,11 @@
 import librosa
+import numpy as np
 
 class Audio:
     def __init__(self,audiopath,sampling_rate,mono=True):
 
         self.mono=mono
-        self.raw, self.sampling_rate =librosa.load(audiopath, sr=sampling_rate, mono=self.mono)
+        self.raw, self.sampling_rate =librosa.load(audiopath, sr=sampling_rate, mono=self.mono)#, dtype=np.float24
 
 
     def get_sampling_rate(self):
@@ -15,3 +16,6 @@ class Audio:
         return self.raw
     def get_duration_sec(self):
         return len(self.raw)/self.sampling_rate
+    def normalize(data):#in the space [-1,1]
+      #return (data - np.min(data)) / (np.max(data) - np.min(data)) #in the space [0,1]
+      return (2*(data-np.min(data))/(np.max(data) - np.min(data)))-1
