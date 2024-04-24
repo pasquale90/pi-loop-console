9ffe248c96b2660e85427e6dd1daa90e6e681683
melissas paschalis <melissaspaschalis@gmail.com>
2024-04-24 14:10:05 +0300

    controls enum added

--- include/controls.h
+++ include/controls.h
@@ -3,4 +3,25 @@
 
 //enumerator
 
+enum{
+    CH1_RECDUB,
+    CH1_STOP,
+    CH2_RECDUB,
+    CH2_STOP,
+    CH3_RECDUB,
+    CH3_STOP,
+    START_ALL,
+    SAVE_JAM,
+    CH1_ARM,
+    CH1_MNTR,
+    CH2_ARM,
+    CH2_MNTR,
+    CH1_EFF1,
+    CH1_EFF2,
+    CH1_EFF3,
+    CH2_EFF1,
+    CH2_EFF2,
+    CH2_EFF3
+};
+
 #endif
