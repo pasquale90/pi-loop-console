3f28db6e994fd456d0a6778fb70d0feaf27d7281
melissas <melissaspaschalis@gmail.com>
2024-07-01 21:04:05 +0100

    finalize buttons for demo

--- include/gpio/buttons.h
+++ include/gpio/buttons.h
@@ -7,13 +7,11 @@
 #define MCP23017_NUM_BUTTONS 16
 #define GPIO_MCP23017_SDA 2
 #define GPIO_MCP23017_SCL 3
-// 7 buttons have been set up directly on the following pins
-#define GPIO_ONOFF 25
-#define GPIO_START_STOP_ALL 18
-#define GPIO_TAP_TEMPO 15
-#define GPIO_SAVE_JAM 19
-#define GPIO_PREV_SESSION 20
-#define GPIO_NEXT_SESSION 12
+// 5 buttons have been set up directly on the following pins
+#define GPIO_ONOFF 4
+#define GPIO_SAVE_JAM 15
+#define GPIO_PREV_SESSION 18
+#define GPIO_NEXT_SESSION 14
 //#define GPIO_SAVE_SESSION 23
 
 #warning "GPIO-buttons included" // include warning
@@ -50,7 +48,7 @@ class UI{
         std::unordered_map<int,Control> control_mapping;
         std::unordered_map<Control,float> time_pressed;
         
-        std::array<int,6> direct_buttons_helper = {GPIO_ONOFF, GPIO_START_STOP_ALL, GPIO_TAP_TEMPO, GPIO_SAVE_JAM, GPIO_PREV_SESSION, GPIO_NEXT_SESSION }; //GPIO_SAVE_JAM
+        std::array<int,4> direct_buttons_helper = {GPIO_ONOFF, GPIO_SAVE_JAM, GPIO_PREV_SESSION, GPIO_NEXT_SESSION }; //GPIO_SAVE_JAM
 
         std::chrono::steady_clock::time_point present,past;
         int time_elapsed;
