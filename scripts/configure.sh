27fb79e9e7a2749bd1adc873a3b70484cf86fd1f
melissaspaschalis <melissaspaschalis@gmail.com>
2024-07-19 17:24:26 +0300

    improve configuration

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -5,22 +5,55 @@ cd $SCRIPT_DIR && cd ..
 source PILOOP_SETTINGS
 
 # if KEYBOARD_DEVICE exists else, set value to it.
-if [[ -z "${KEYBOARD_DEVICE}" ]];then 
-    # detect keyboard device..
-    kbdEvents=($(ls /dev/input/by-path | grep "event-kbd"))  
-    echo ${kbdEvents}
-    for forCounter in "${kbdEvents[@]}"
-    do
-        KEYBOARD_DEVICE=$(readlink --canonicalize "/dev/input/by-path/${forCounter}")     
-    done
-    echo ${KEYBOARD_DEVICE}
+if [ "${PILOOP_MODE}" = "PC" ];then
+    if [[ -z "${KEYBOARD_DEVICE}" ]];then 
+        # detect keyboard device..
+        kbdEvents=($(ls /dev/input/by-path | grep "event-kbd"))  
+        # echo ${kbdEvents}
+        for forCounter in "${kbdEvents[@]}"
+        do
+            KEYBOARD_DEVICE=$(readlink --canonicalize "/dev/input/by-path/${forCounter}")     
+        done
+        # echo ${KEYBOARD_DEVICE}
+    fi
 fi
 
 # same with i2c bus
-if [[ -z "${I2C_BUS_ADDR}" ]];then 
-    echo "i have to find it."
-    I2C_BUS_ADDR="0x20" # known value -> FIX
+if [ "${PILOOP_MODE}" = "RPI" ];then
+    if [[ -z "${I2C_BUS_ADDR}" ]];then 
+        : '     # find the i2c bus address. Parse i2c detect -y 1 output
+        i2c_out=`i2cdetect -y 1`
+        n1=0
+        n2=0
+        b=0
+        
+        for (( i=0; i<${#i2c_out}; i++ )); do
+
+            curr=${i2c_out:$i:1}
+            next=${i2c_out:$i+1:1}
+
+            if [  "$curr" = ' ' ] && [  "$next" = ' ' ];then 
+                    b=0
+            fi
+
+            if [ "$b" = 0 ] && [ "$curr" = ':' ];then
+                b=1
+            fi
+
+            if [ "$b" = 0 ] && [ cur!=" " ] || [ curr!="-" ];then
+                if [ b==0 ] && [ $next != " " ] || [ $next != "-" ];then
+                    n1=$(($curr + 1))
+                    n2=$(($next + 1))
+                fi
+            fi
+            # echo "${i2c_out:$i:1} ${i2c_out:$i+1:1}"
+        done
+        echo "$n1 $n2"
+        '
+        I2C_BUS_ADDR="0x20" # dumb value -> FIX LATTER
+    fi
 fi
+
 
 export PILOOP_MODE
 export AUDIO_DEVICE
