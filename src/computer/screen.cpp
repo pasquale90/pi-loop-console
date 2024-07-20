59e1924bf12ff75079ce9bb94f7c12ef93d4cc6d
blawk <melissaspaschalis@gmail.com>
2024-07-20 05:30:39 +0300

    improve visual identation

--- src/computer/screen.cpp
+++ src/computer/screen.cpp
@@ -85,7 +85,7 @@ void Screen::tick_tock(int intonation){
 		printf("\033[%d;%dH\33[2K\r", curr_line-1, 0);
 	}
 	printf("\033[%d;%dH", curr_line++, 0);
-	std::cout<<"                       | "<<metro_mapping[intonation]<<" |\n";
+	std::cout<<"                         | "<<metro_mapping[intonation]<<" |\n";
 }
 
 void Screen::_setupTextMapping(){
@@ -168,13 +168,13 @@ void Screen::_display_text(){
     <<"| "<<toggle_text_mapping[IN1_EFF1][toggle_text_states[IN1_EFF1].load()]<<" "<<toggle_text_mapping[IN1_EFF2][toggle_text_states[IN1_EFF2].load()]<<" "<<toggle_text_mapping[IN1_EFF3][toggle_text_states[IN1_EFF3].load()]<<"    "<<toggle_text_mapping[IN2_EFF1][toggle_text_states[IN2_EFF1].load()]<<" "<<toggle_text_mapping[IN2_EFF2][toggle_text_states[IN2_EFF2].load()]<<" "<<toggle_text_mapping[IN2_EFF3][toggle_text_states[IN2_EFF3].load()]<<" |  |     #tracks     |\n"
     <<"|    "<<toggle_text_mapping[IN1_ARM][toggle_text_states[IN1_ARM].load()]<<"   "<<toggle_text_mapping[IN1_MNTR][toggle_text_states[IN1_MNTR].load()]<<"       "<<toggle_text_mapping[IN2_ARM][toggle_text_states[IN2_ARM].load()]<<"   "<<toggle_text_mapping[IN2_MNTR][toggle_text_states[IN2_MNTR].load()]<<"   |  |  "<<mixer_states[0].load()<<"     "<<mixer_states[1].load()<<"     "<<mixer_states[2].load()<<"  |\n"
     <<"------------------------------------  -------------------\n\n"
-    <<" Current               PILOOP-CONSOLE          SAVE_JAM\n"
+    <<" Current              PILOOP-CONSOLE           SAVE_JAM\n"
     <<" "<<"Session:"<<num_session.load()<<"                 "<<piloop_state<<"              "<<jam_save_state<<" \n\n"
     <<"------------------------- LOOPER -------------------------\n"
     <<"|  Start       Channel 1       Channel 2       Channel 3 |\n"
     <<"| "<<"Stop ALL"<<"     "<<looper_text_mapping[0][looper_states[0][0].load()]<<"  "<<looper_text_mapping[1][looper_states[0][1].load()]<<"       "<<looper_text_mapping[0][looper_states[1][0].load()]<<"  "<<looper_text_mapping[1][looper_states[1][1].load()]<<"       "<<looper_text_mapping[0][looper_states[2][0].load()]<<"  "<<looper_text_mapping[1][looper_states[2][1].load()]<<" |\n"
     <<"----------------------------------------------------------\n"
-	<<"---------------------- Metronome -------------------------\n"<<std::endl;
+	<<"------------------------ Metronome -----------------------\n"<<std::endl;
 
 	std::cout << display_text.str()<<std::endl;
 
