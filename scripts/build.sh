d8d6231a2e786d43c8c045fb69a384671cae5d9c
melissas <melissaspaschalis@gmail.com>
2024-08-01 11:05:03 +0100

    minor fix in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,8 @@
 #!bin/bash
 
-if [[ -z "${PILOOP_MODE}" ]];then 
+if ! [[ -z "${PILOOP_MODE}" ]];then 
   echo "configure first"
+  exit -1
 fi
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
