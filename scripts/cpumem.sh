5697550b3706dfd0a351a79a83468e3ae0c47d53
melissas <melissaspaschalis@gmail.com>
2024-08-02 16:28:44 +0100

    added cpumem and run scripts

---
+++ scripts/cpumem.sh
@@ -0,0 +1,38 @@
+#!bin/bash
+
+mbool=false
+
+clear
+
+echo $pid
+
+while true
+do
+    
+    pid=$(pgrep -x piloop)
+
+    if [ -n "${pid}" ];then
+        clear
+        break;
+    else
+        if ! $mbool ; then
+            printf "waiting for PiLoop process to start"
+            mbool=true
+        else
+            echo -ne "."
+        fi
+    fi
+
+    sleep 2
+done
+
+printf '\nPILOOP resources usage:\n%%cpu |  %%mem  | process\n'
+
+while kill -0 $pid 2> /dev/null;
+do
+    resources=$(top -b -n 2 -d 0.2 -p $pid | tail -1 | awk '{print $9 " |  " $10 "  | " $12 }')
+    echo -ne "$resources\033[0K\r"
+    sleep 1
+done
+clear
+exit
